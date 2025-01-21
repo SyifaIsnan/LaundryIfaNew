2c4ba7985f8d486bd5e9c3d53ab6bd25ce019c4c
SyifaIsnan <syifaisnantyana@gmail.com>
2025-01-21 11:37:27 +0700

    proses

--- LaundryIfaNew/LaundryIfaNew/Form1.cs
+++ LaundryIfaNew/LaundryIfaNew/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,46 @@ namespace LaundryIfaNew
         public Form1()
         {
             InitializeComponent();
+        }
+
+        SqlConnection conn = Properti.conn;
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.PasswordChar = checkBox1.Checked ? '\0' : '*';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Properti.validasi(this.Controls))
+                {
+                    MessageBox.Show("Inputan tidak boleh kosong!");
+                }
+                else
+                {
+                    var mess = MessageBox.Show("Apakah data yang ingin diinput sudah benar?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (mess == DialogResult.Yes) {
+                        SqlCommand cmd = new SqlCommand("select * from [User] where email = @email and password = @password", conn);
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Parameters.AddWithValue("@email", textBox1.Text);
+                        cmd.Parameters.AddWithValue("@password", textBox2.Text);
+                        SqlDataReader rd = cmd.ExecuteReader();
+                        if (rd.Read())
+                        {
+                            this.Hide();
+                            string namauser = rd["namauser"].ToString();
+
+                        }
+                        
+                    }   
+                }
+            }
+            catch
+            {
+               
+            }
         }
     }
 }
---
+++ LaundryIfaNew/LaundryIfaNew/Properti.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaundryIfaNew
+{
+    internal class Properti
+    {
+        //koneksi
+        public static SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-18L8S2S;Initial Catalog=LaundrySyifa;Integrated Security=True");
+
+        //vaidasi
+        public static bool validasi(Control.ControlCollection container, TextBoxBase kosong = null)
+        {
+            foreach (Control c in container)
+            {
+                if (c is TextBoxBase textBox && string.IsNullOrEmpty(textBox.Text) && textBox != kosong) 
+                { 
+                    return true;
+                }
+            } return false;
+        }
+
+        //enkripsi
+        public static string enkripsi(string input)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("2x"));
+            } 
+            
+            return sb.ToString();
+        }
+        
+    }
+}
